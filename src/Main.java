8375ea84c776e3c8869dd79142d7689acbea6974
Seojeong Woo <smilejeong@handong.ac.kr>
2023-09-07 20:09:19 +0900

    화면 출력

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main{
     public static void main(String[] args) {
-
+        System.out.println("*** 영단어 마스터 ***");
+        System.out.println();
+        System.out.println("****************");
+        System.out.println("1. 모든 단어 보기");
+        System.out.println("2. 수준별 단어 보기");
+        System.out.println("3. 단어 검색");
+        System.out.println("4. 단어 추가");
+        System.out.println("5. 단어 수정");
+        System.out.println("6. 단어 삭제");
+        System.out.println("7. 파일 저장");
+        System.out.println("0. 나가기");
+        System.out.println("****************");
+        System.out.print("=> 원하는 메뉴는? ");
     }
 }
