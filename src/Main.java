d7d1fad81f7b60769b7b86c56c81f73360c5c9fb
Seojeong Woo <smilejeong@handong.ac.kr>
2023-09-13 22:29:05 +0900

    단어장 파일 형식 변경

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
 
+    // main 함수는 프로그램의 시작점이다
     public static void main(String[] args) throws IOException {
         BufferedReader reader;
         BufferedWriter writer;
+        Scanner scanner = new Scanner(System.in);
 
+        // cout << "*** 영단어 마스터 ***");
         System.out.println("*** 영단어 마스터 ***");
         System.out.println();
 
-        int input = 1;
-        while (input > 0 && input < 8) {
+        int menuNumber = 1;
+
+        while (menuNumber > 0 && menuNumber < 8) {
             printMenu();
 
-            Scanner scanner = new Scanner(System.in);
-            input = scanner.nextInt();
-            if (input == 0) { // 나가기
+            menuNumber = scanner.nextInt();
+
+            if (menuNumber == 0) { // 나가기
                 System.out.println();
                 System.out.println("프로그램 종료! 다음에 만나요~");
-            } else if (input == 1) { // 모든 단어 보기
+            } else if (menuNumber == 1) { // 모든 단어 보기
                 reader = new BufferedReader(new FileReader("dictionary.txt"));
 
                 System.out.println();
@@ -36,36 +41,46 @@ public class Main {
                 }
                 System.out.println("--------------------");
                 System.out.println();
-            } else if (input == 4) { // 단어 추가
+            } else if (menuNumber == 4) { // 단어 추가
                 // 단어장 만들기
-                reader = new BufferedReader(new FileReader("dictionary.txt"));
+                FileReader fileReader = new FileReader("dictionary.txt");
+                reader = new BufferedReader(fileReader);
+
                 List<String> lines = new ArrayList<>();
                 while (true) {
                     String line = reader.readLine();
                     if (line == null) {
                         break;
                     }
-                    lines.add(line);
+                    lines.add(line.substring(line.indexOf('*')));
                 }
+
+
 
                 // 단어 입력받기
                 System.out.println();
                 System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
                 scanner.nextLine(); // 앞에 남아있는 엔터 버리기
                 String levelAndWord = scanner.nextLine(); // 난이도 + 단어
+                String[] split = levelAndWord.split(" "); // 3 hello => [3, hello]
+                int level = Integer.parseInt(split[0]);
+                String word = split[1];
 
                 System.out.print("뜻 입력 : ");
                 String meaning = scanner.nextLine(); // 뜻
 
                 // 단어장에 단어 추가하기
-                lines.add(levelAndWord + " " + meaning);
-
-                // TODO 정렬하기
+                String newLine = "";
+                for (int i=0; i<level; i++) {
+                    newLine += "*";
+                }
+                newLine += " " + word + " " + meaning;
+                lines.add(newLine);
 
                 // 다시 파일에 쓰기
                 writer = new BufferedWriter(new FileWriter("dictionary.txt", false));
                 for (int i=0; i<lines.size(); i++) {
-                    writer.write(lines.get(i) + "\n");
+                    writer.write((i+1) + " " + lines.get(i) + "\n");
                 }
                 writer.flush(); // 버스 출발
 
